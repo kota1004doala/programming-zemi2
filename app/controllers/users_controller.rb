6020bebb132c20c80ac4c81bfd4f6b43ef6cbe7a
kota1004doala <mac1004kota@gmale.com>
2021-09-02 02:55:07 +0000

    basic signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   # GET /users or /users.json
   def index
     @users = User.all
@@ -13,6 +17,16 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)  
+    if @user.save
+      # 保存の成功をここで扱う。
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   # GET /users/1/edit
